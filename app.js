820b63ff2c0712b98dfa7b1eece8923755fa08ca
voke <voke74ji@gmail.com>
2022-06-11 12:01:55 +0800

    project init

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const exphbs = require('express-handlebars')
+
+const routes = require('./routes')
+
+const app = express()
+const port = 3000
+
+app.engine('hbs', exphbs.engine({ defaultLayout: 'main', extname: 'hbs' }))
+app.set('view engine', 'hbs')
+
+app.use(express.static('public'))
+app.use(routes)
+
+app.listen(port, () => {
+  console.log(`Listening on http://localhost:${port}`)
+})
